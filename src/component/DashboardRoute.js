4a0ef939a199b1cd5f3403fcf117d6fd43c8aaf8
Blaspace <115324109+Blaspace@users.noreply.github.com>
2024-01-04 05:26:39 -0800

    hello

--- src/component/DashboardRoute.js
+++ src/component/DashboardRoute.js
@@ -6,30 +6,6 @@ import AuthContext from "../context/AuthContext";
 import UsersContext from "../context/UsersContext";
 
 function DashboardRoute() {
-  const { auth, uri } = useContext(AuthContext);
-  const { setUser, setUsers } = useContext(UsersContext);
-
-  useEffect(() => {
-    fetch(`${uri}/getuser`, {
-      method: "POST",
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then((data) => setUser(data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  useEffect(() => {
-    fetch(`${uri}/getusers`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => setUsers(data))
-      .catch((err) => console.log(err));
-  }, []);
   return (
     <>
       <DashboardNav />
--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -12,6 +12,28 @@ export function UsersProvider({ children }) {
   const { uri } = useContext(AuthContext);
 
   useEffect(() => {
+    fetch(`${uri}/getuser`, {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => setUser(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    fetch(`${uri}/getusers`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
     fetch(`${uri}/getassignment`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
